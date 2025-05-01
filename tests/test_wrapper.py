625fff6d6cfc9ec2d45dd56e6e97ef04e30f1642
Martin Landa <landa.martin@gmail.com>
2025-05-01 14:16:40 +0200

    fix reference data

--- tests/test_wrapper.py
+++ tests/test_wrapper.py
@@ -5,14 +5,14 @@ from osgeo import ogr
 from gdal_jvf_dtm_wrapper import GdalJvfDtmWrapper
 
 ref_layers = {
-    'Budovy#Objekt_budovy#010000000104_Budova': ('Point', 11, 6), # geom type, field count, feature count
-    'Rekreační,_kulturní_a_sakrální_stavby#Stavba_kulturní,_sakrální#010000015901_Drobná_kulturní_stavba': ('Point', 15, 24),
-    'Konstrukční_prvky_objektů#Základní_konstrukční_prvek#010000029902_Hranice_budovy': ('Curve', 14, 64),
-    'Konstrukční_prvky_objektů#Základní_konstrukční_prvek#010000030402_Hranice_dopravní_stavby_nebo_plochy': ('Curve', 15, 236),
-    'Konstrukční_prvky_objektů#Základní_konstrukční_prvek#010000030102_Hranice_schodiště': ('Curve', 15, 112),
-    'Součásti_a_příslušenství_staveb#Stavba_společná_pro_více_skupin#010000016202_Plot': ('Curve', 16, 21),
-    'Geodetické_prvky#Podrobný_bod#010000021801_Podrobný_bod_zps': ('Point', 14, 893),
-    'Vodstvo,_vegetace_a_terén#Terénní_útvar#010000021702_Terénní_hrana': ('Curve', 15, 55)
+    'Budovy#Objekt_budovy#010000000104_Budova': ('Point', 10, 6), # geom type, field count, feature count
+    'Rekreační,_kulturní_a_sakrální_stavby#Stavba_kulturní,_sakrální#010000015901_Drobná_kulturní_stavba': ('Point', 14, 24),
+    'Konstrukční_prvky_objektů#Základní_konstrukční_prvek#010000029902_Hranice_budovy': ('Curve', 13, 64),
+    'Konstrukční_prvky_objektů#Základní_konstrukční_prvek#010000030402_Hranice_dopravní_stavby_nebo_plochy': ('Curve', 14, 236),
+    'Konstrukční_prvky_objektů#Základní_konstrukční_prvek#010000030102_Hranice_schodiště': ('Curve', 14, 112),
+    'Součásti_a_příslušenství_staveb#Stavba_společná_pro_více_skupin#010000016202_Plot': ('Curve', 15, 21),
+    'Geodetické_prvky#Podrobný_bod#010000021801_Podrobný_bod_zps': ('Point', 13, 893),
+    'Vodstvo,_vegetace_a_terén#Terénní_útvar#010000021702_Terénní_hrana': ('Curve', 14, 55)
 }
 
 ref_layer = {
